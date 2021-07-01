71ab62df787676ef5668d63263524b8f9bd6c4f0
Max Manayan <maximillian521@gmail.com>
2021-07-01 13:31:28 -0500

    higher order functions lecture complete

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
-function sayHello() {
-  return function () {
-    return "Hello Max";
-  };
-}
+//// Functions are First-Class Citizens
+// function sayHello() {
+//   return function () {
+//     return "Hello Max";
+//   };
+// }
 
-let fn = sayHello();
-let message = fn();
+// let fn = sayHello();
+// let message = fn();
 
-console.log(message);
+// console.log(message);
+
+// // Higher Order Functions
+
+// let numbers = [1, 2, 3];
+
+// // .map() and setTimeout() are higher order functions... take another function as an argument
+// numbers.map((number) => number * 2);
+
+// setTimeout(() => console.log("Hello"), 1000);
