e154efc509fb5995559b2958cad8e7c0e45d6064
Max Manayan <maximillian521@gmail.com>
2021-07-04 18:35:22 -0500

    subscribed to store

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 import store from "./store";
 
+// UI components should subscribe to the store so they get notified when the store changes
+
+const unsubscribe = store.subscribe(() => {
+  console.log("Store Changed!", store.getState());
+});
+
 store.dispatch({
   type: "bugAdded",
   payload: {
     description: "Bug1",
   },
 });
+
+unsubscribe();
 
 store.dispatch({
   type: "bugRemoved",
