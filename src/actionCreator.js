8ffdc3b322d3cb8aaa4b53e13742615cd177e1ec
Max Manayan <maximillian521@gmail.com>
2021-07-05 08:43:28 -0500

    redux first hour of lessons complete

--- src/actionCreator.js
+++ src/actionCreator.js
@@ -3,13 +3,20 @@ import * as actions from "./actionTypes";
 export const bugAdded = (description) => ({
   type: actions.BUG_ADDED,
   payload: {
-    description: "Bug1",
+    description,
   },
 });
 
 export const bugRemoved = (id) => ({
   type: actions.BUG_REMOVED,
   payload: {
-    id: 1,
+    id,
+  },
+});
+
+export const bugResolved = (id) => ({
+  type: actions.BUG_RESOLVED,
+  payload: {
+    id,
   },
 });
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import store from "./store";
-import { bugAdded, bugRemoved } from "./actionCreator";
+import { bugAdded, bugRemoved, bugResolved } from "./actionCreator";
 
 // UI components should subscribe to the store so they get notified when the store changes
 const unsubscribe = store.subscribe(() => {
@@ -7,6 +7,8 @@ const unsubscribe = store.subscribe(() => {
 });
 
 store.dispatch(bugAdded("Bug1"));
+
+store.dispatch(bugResolved(1));
 
 unsubscribe();
 
