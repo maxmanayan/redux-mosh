b999435ef0d81bbd04eacea3b867f94f029db274
Max Manayan <maximillian521@gmail.com>
2021-07-04 21:15:06 -0500

    action creator added

--- src/index.js
+++ src/index.js
@@ -1,26 +1,15 @@
 import store from "./store";
-import * as actions from "./actionTypes";
+import { bugAdded, bugRemoved } from "./actionCreator";
 
 // UI components should subscribe to the store so they get notified when the store changes
-
 const unsubscribe = store.subscribe(() => {
   console.log("Store Changed!", store.getState());
 });
 
-store.dispatch({
-  type: actions.BUG_ADDED,
-  payload: {
-    description: "Bug1",
-  },
-});
+store.dispatch(bugAdded("Bug1"));
 
 unsubscribe();
 
-store.dispatch({
-  type: actions.BUG_REMOVED,
-  payload: {
-    id: 1,
-  },
-});
+store.dispatch(bugRemoved(1));
 
 console.log(store.getState());
